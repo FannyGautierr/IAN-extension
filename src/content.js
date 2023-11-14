83dd16598aa630fc2da3051b0f653dcded49b745
Fanny Gautier <fannygautier92@gmail.com>
2023-11-14 10:52:00 +0100

    start font

--- src/content.js
+++ src/content.js
@@ -1,43 +1,15 @@
-
-
-
-let repositoryId = document.querySelector('meta[name="octolytics-dimension-repository_id"]').content
-console.log(repositoryId)
-save(repositoryId)
-
-
-function save(repositoryId) {
-    console.log('repo Id :' + repositoryId)
-    chrome.storage.local.get(['apiKey'], function (result) {
-        if (result.apiKey) {
-
-                console.log(apiKey)
-                chrome.storage.local.set({apiKey: apiKey}, function () {
-                    console.log('Value is set to ' + apiKey)
-                })
-                console.log('fetching...')
-                fetch(`https://127.0.0.1:8000/api/repository/add/${repositoryId}`, {
-                    method: 'POST',
-                    headers: {
-                        'X-Access-Token': apiKey,
-                    }
-                }).then(response => {
-                    if (response.status === 200) {
-                        console.log('Success')
-                    } else {
-                        console.log('Error')
-                    }
-                }).catch(error => {
-                    console.log(error)
-                })
-            } else {
-                console.log('You must have an API key to continue')
-            }
-
-
-
-    })
+function changeFontSize(delta) {
+    const elements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a, li, button, input, textarea, select, option, label');
+    elements.forEach(element => {
+        const currentSize = parseInt(window.getComputedStyle(element).fontSize);
+        element.style.fontSize = `${currentSize + delta}px`;
+    });
 }
 
-
-
+chrome.runtime.onMessage.addEventListener((message, sender, sendResponse) => {
+    if (message.action === "increaseFontSize") {
+        changeFontSize(1); // Increase font size by 1px
+    } else if (message.action === "decreaseFontSize") {
+        changeFontSize(-1); // Decrease font size by 1px
+    }
+});
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,11 @@
+document.getElementById('increase').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "increaseFontSize"});
+    });
+});
 
+document.getElementById('decrease').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "decreaseFontSize"});
+    });
+});
